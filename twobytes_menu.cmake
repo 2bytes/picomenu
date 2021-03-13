03dc02269f3519c1a0cf35d500a27f7bb1d56297
Hamid Elaosta <hamid@2byt.es>
2021-03-13 19:04:54 +0000

    Tidy up project structure Update cmake configuration, hopefully it's better now (I've not used cmake before so I'm winging it >_<)

--- twobytes_menu.cmake
+++ twobytes_menu.cmake
@@ -15,12 +15,12 @@ add_subdirectory($ENV{PIM_SDK_PATH}/libraries/pico_graphics pico_graphics)
 add_subdirectory($ENV{PIM_SDK_PATH}/libraries/pico_display pico_display)
 
 target_sources(twobytes_menu INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/twobytes_menu.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/menu_base.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/menu_list.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/menu_action.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/src/twobytes_menu.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/src/menu_base.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/src/menu_list.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/src/menu_action.cpp
 )
 
-target_include_directories(twobytes_menu INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+target_include_directories(twobytes_menu INTERFACE ${CMAKE_CURRENT_LIST_DIR}/include)
 
 target_link_libraries(twobytes_menu INTERFACE pico_stdlib hardware_spi st7789 pico_display pico_graphics)
